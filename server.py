b69fd5097388079cf1792b2693d2b7758e1d9f47
Mike Bergstrom <mikedberg@hotmail.com>
2017-06-08 15:14:56 -0700

    working project

--- server.py
+++ server.py
@@ -18,9 +18,30 @@ def process_money():
     goldresp = random.randint(10,20)
     print goldresp
     session['goldCount'] = session['goldCount'] + goldresp
-    # newact = "Earned {} golds from the farm! {}"format(goldresp, datetime.datetime.now())
-    # print newact
     session['activities'].append("Earned "+ str(goldresp) + " golds from the farm! " + str(datetime.datetime.now()))
     return render_template('index.html')
+ elif request.form['building'] == 'cave':
+    goldresp = random.randint(5,10)
+    print goldresp
+    session['goldCount'] = session['goldCount'] + goldresp
+    session['activities'].append("Earned "+ str(goldresp) + " golds from the cave! " + str(datetime.datetime.now()))
+    return render_template('index.html')
+ elif request.form['building'] == 'house':
+    goldresp = random.randint(2,5)
+    print goldresp
+    session['goldCount'] = session['goldCount'] + goldresp
+    session['activities'].append("Earned "+ str(goldresp) + " golds from the house! " + str(datetime.datetime.now()))
+    return render_template('index.html')
+ elif request.form['building'] == 'casino':
+    goldresp = random.randint(-50,50)
+    print goldresp
+    if goldresp >=0:
+        session['goldCount'] = session['goldCount'] + goldresp
+        session['activities'].append("Entered a casino and won "+ str(goldresp) + " golds! Yipee!! " + str(datetime.datetime.now()))
+        return render_template('index.html')
+    elif goldresp < 50:
+        session['goldCount'] = session['goldCount'] + goldresp
+        session['activities'].append("Entered a casino and lost "+ str(goldresp) + " golds...ouch! " + str(datetime.datetime.now()))
+        return render_template('index.html')
 
 app.run(debug=True)
