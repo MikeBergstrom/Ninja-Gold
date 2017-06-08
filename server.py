dfc629d5bd0a313bf34d5a45c991d7a5a49ff147
Mike Bergstrom <mikedberg@hotmail.com>
2017-06-08 15:06:56 -0700

    got activity list working

--- server.py
+++ server.py
@@ -1,12 +1,26 @@
 from flask import Flask, render_template, request, redirect, session
 import random
+import datetime
 app = Flask(__name__)
 app.secret_key= 'ThisIsSecret'
 
 @app.route('/')
+def index():
+    session['goldCount'] = 0
+    session['activities'] = []
+#   session['stuff'] = session['stuff']
+    print session['goldCount']
+    return render_template("index.html")
 
-def index(): 
-  return render_template("index.html")
+@app.route('/process_money', methods=['post'])
+def process_money():
+ if request.form['building'] == 'farm':
+    goldresp = random.randint(10,20)
+    print goldresp
+    session['goldCount'] = session['goldCount'] + goldresp
+    # newact = "Earned {} golds from the farm! {}"format(goldresp, datetime.datetime.now())
+    # print newact
+    session['activities'].append("Earned "+ str(goldresp) + " golds from the farm! " + str(datetime.datetime.now()))
+    return render_template('index.html')
 
-
-  app.run(debug=True)
+app.run(debug=True)
